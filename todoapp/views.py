47011bbfbfcf7c142f2c9040ca6f0e085f771eb1
Max <kyzmenko.official@gmail.com>
2022-12-03 16:57:38 +0200

    Create the To-Do List

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,14 @@
+from .models import TodoListItem
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponseRedirect
 
 
 def todoappView(request):
-    return render(request, 'todolist.html')
+    all_todo_items = TodoListItem.objects.all()
+    return render(request, 'todolist.html', {'all_items': all_todo_items})
+
+def addTodoView(request):
+    x = request.POST['content']
+    new_item = TodoListItem(content=x)
+    new_item.save()
+    return HttpResponseRedirect('/todoapp/')
